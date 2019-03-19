ec8b79a7e621698453be5111e0badcf1c867fb15
Steve Lanier <stevekentlanier@gmail.com>
2019-03-18 17:00:37 -0700

    MVP complete

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderContent = () => (
+  <p className="header-content">
+    REACT IS RRRAAWWWWRRSOME!!!!
+  </p>
+);
+
+export default HeaderContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,18 @@
 import React from 'react';
+import HeaderContent from './HeaderContent';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+  return (
+    <div className="header-title-wrapper">
+      <div className="header-title">
+        Lambda School Peeps
+        <span className="time-stamp">25 or 6 to 4</span>
+        <HeaderContent />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderTitle;
